c0c0ed418d665210d5ca571d5a5fd214494f9f43
Armin <glavovic.armin@gmail.com>
2022-02-25 06:02:32 -0500

    get request for /api/days to generate days

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,8 @@ import DayList from "./DayList";
 import "styles/Application.scss";
 import "components/Appointment"
 import Appointment from "components/Appointment";
+import axios from "axios";
+import { useEffect } from "react";
 
 const appointments = [
   {
@@ -43,27 +45,19 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function Application()
  {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([])
+
+  useEffect(() => {
+    axios.get("/api/days")
+    .then(function (response) {
+      setDays([...response.data])
+    });
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
